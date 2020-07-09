67e918b1e939f3da408852cf5810a1315f63851b
Patricio <patricio.montes@codeicus.com>
2020-07-08 21:17:49 -0300

    CODGEN-65. Fixed: logger

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from ClientsImpl.google_drive_impl import GoogleDriveImpl
 from ServicesImpl.file_manager_impl import FileManagerImpl
 
 expected_connection = ExpectedConnection()
-#TODO: Evaluar mejor forma de recibir al cronear.  
+#TODO: Evaluar mejor forma de recibir al cronear. Y controlar las entradas. 
 print("Tipo de Host 1-SQL 2-Oracle 3-Sybase 4-Postgress 5-MySQL")
 expected_connection.typehost = input()
 print("Host:")
@@ -30,20 +30,25 @@ logging.info("Data Source pretendido." +
 
 db_connection = factory_connection.create(expected_connection.get_db_type(int(expected_connection.typehost)))
 
-logging.debug("Estableciendo conexión con la base de datos")
+logging.debug("Estableciendo conexión con la base de datos...")
 conn = pyodbc.connect(db_connection.build_connection(expected_connection), autocommit=True)
 cursor = conn.cursor()
 
-logging.debug("Obteniendo directorio de backup")
+logging.debug("Obteniendo directorio de backup...")
 file_manager = FileManagerImpl()
 absolute_backup_path = file_manager.get_absolute_path_backup(expected_connection.database, expected_connection.get_db_type(int(expected_connection.typehost)))
 
-logging.debug("Ejecutando query a la base de datos")
+logging.debug("Ejecutando query a la base de datos...")
 logging.info("Script:" + db_connection.build_query_backup(expected_connection.database, absolute_backup_path))
 cursor.execute(db_connection.build_query_backup(expected_connection.database, absolute_backup_path))
 
 print("BackUp finalizado. Enter para continuar")
 input()
-
+print(file_manager.get_backup_size(absolute_backup_path))
+input()
 google_drive_manager = GoogleDriveImpl()
-google_drive_manager.upload_resumable_file(absolute_backup_path)
+# file size > 5000 KB carga reanudable. file size < 5000 KB carga simple
+if file_manager.get_backup_size(absolute_backup_path) > 5000:
+    google_drive_manager.upload_resumable_file(absolute_backup_path)
+else:
+    google_drive_manager.upload_simple_file(absolute_backup_path)
